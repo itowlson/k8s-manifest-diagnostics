24a964aeddc2e63834e91238b03c9b6ba7fe62c8
itowlson <github@hestia.cc>
2020-04-01 15:43:11 +1300

    Initial scaffold

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+## Unreleased
+
+* Goal for 0.0.1: write linters in terms of k8s-manifest-parser constructs and map to VS Code
---
+++ ts/test/placeholder.ts
@@ -0,0 +1,10 @@
+import * as assert from 'assert';
+
+import * as t from '../src/index';
+
+describe('the package', () => {
+    it('should work', () => {
+        const result = t.hello('world');
+        assert.equal('Hello, world', result);
+    });
+});
