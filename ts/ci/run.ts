b82bf69f1002101911ed9da18ae46180226256d5
itowlson <itowlson@microsoft.com>
2020-04-03 11:59:44 +1300

    Set up test and CI bits for VS Code dependency (#1)

---
+++ ts/ci/run.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+
+import { downloadAndUnzipVSCode, runTests } from 'vscode-test';
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+        // The path to the extension test script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './tests');
+
+        const vscodeExecutablePath = await downloadAndUnzipVSCode('1.43.2');
+        // const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+        // Download VS Code, unzip it and run the integration test
+        const exitCode = await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+        console.log(`Test runner exited with code ${exitCode}`);
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
+}
+
+main();
