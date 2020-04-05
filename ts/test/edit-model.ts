2499423a60384ce529a06ec026e9fda4fecea9ef
itowlson <github@hestia.cc>
2020-04-06 11:24:37 +1200

    Added a test for JSON naive insert

--- ts/test/edit-model.ts
+++ ts/test/edit-model.ts
@@ -9,6 +9,7 @@ import { withTempFileCopy } from './testutils/tempfile';
 
 const TEST_DATA_DIR = path.join(__dirname, '../../ts/test/data');
 const SIMPLE_MANIFEST = 'simple-manifest.yaml';
+const SIMPLE_MANIFEST_JSON = 'simple-manifest.json';
 
 function lineText(document: vscode.TextDocument, containing: string): string {
     const index = document.getText().indexOf(containing);
@@ -56,5 +57,14 @@ describe('inserting text into a YAML document', () => {
         kd.combine(wsedit, doc, { kind: 'insert', at: insertAt, text: 'saferegistry.io/' });
         await vscode.workspace.applyEdit(wsedit);
         assert.equal('image: saferegistry.io/zotifier:1.0.0', lineText(doc, 'image: '));
+    });
+});
+
+describe('inserting text into a JSON document', () => {
+    it('should work on the happy path', SIMPLE_MANIFEST_JSON, async (doc, wsedit) => {
+        const insertAt = insertIndex(doc, `"image": "`, `zotifier`);
+        kd.combine(wsedit, doc, { kind: 'insert', at: insertAt, text: 'saferegistry.io/' });
+        await vscode.workspace.applyEdit(wsedit);
+        assert.equal('"image": "saferegistry.io/zotifier:1.0.0"', lineText(doc, '"image": '));
     });
 });
