09a8eb47c4219972dda909ff08aa587ac35f7189
itowlson <github@hestia.cc>
2020-04-06 13:24:33 +1200

    Renamed a constant

--- ts/test/edit-model.ts
+++ ts/test/edit-model.ts
@@ -8,7 +8,7 @@ import * as kd from '../src/index';
 import { withTempFileCopy } from './testutils/tempfile';
 
 const TEST_DATA_DIR = path.join(__dirname, '../../ts/test/data');
-const SIMPLE_MANIFEST = 'simple-manifest.yaml';
+const SIMPLE_MANIFEST_YAML = 'simple-manifest.yaml';
 const SIMPLE_MANIFEST_JSON = 'simple-manifest.json';
 
 function lineText(document: vscode.TextDocument, containing: string): string {
@@ -52,20 +52,20 @@ function it(title: string, testFileName: string, fn: (doc: vscode.TextDocument, 
 }
 
 describe('inserting text into a YAML document', () => {
-    it('should work on the happy path', SIMPLE_MANIFEST, async (doc, wsedit) => {
+    it('should work on the happy path', SIMPLE_MANIFEST_YAML, async (doc, wsedit) => {
         const insertAt = insertIndex(doc, 'image: ', 'zotifier');
         kd.combine(wsedit, doc, { kind: 'insert', at: insertAt, text: 'saferegistry.io/' });
         await vscode.workspace.applyEdit(wsedit);
         assert.equal('image: saferegistry.io/zotifier:1.0.0', lineText(doc, 'image: '));
     });
-    it('should be a no-op before start of document', SIMPLE_MANIFEST, async (doc, wsedit) => {
+    it('should be a no-op before start of document', SIMPLE_MANIFEST_YAML, async (doc, wsedit) => {
         const before = doc.getText();
         kd.combine(wsedit, doc, { kind: 'insert', at: -3, text: 'saferegistry.io/' });
         await vscode.workspace.applyEdit(wsedit);
         const after = doc.getText();
         assert.equal(after, before);
     });
-    it('should be a no-op after end of document', SIMPLE_MANIFEST, async (doc, wsedit) => {
+    it('should be a no-op after end of document', SIMPLE_MANIFEST_YAML, async (doc, wsedit) => {
         const before = doc.getText();
         kd.combine(wsedit, doc, { kind: 'insert', at: 1000, text: 'saferegistry.io/' });
         await vscode.workspace.applyEdit(wsedit);
